6841506c9f81cac6e764939d380abdeab1c963de
casimanodes <138213547+casimanodes@users.noreply.github.com>
2024-05-24 10:21:18 +0200

    Update schlagball.js

--- schlagball.js
+++ schlagball.js
@@ -1,22 +1,22 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.querySelector('.bottom_navigation');
-    const navbarHeight = navbar.offsetHeight;
-    const stickyThreshold = window.innerHeight - navbarHeight; // Adjust if necessary
-    const mainIMG = document.querySelector('main img');
-    const main = document.querySelector('main');
+const trainings = document.getElementById('training');
+const monday = document.getElementById('monday');
+const friday = document.getElementById('friday');
+const sunday = document.getElementById('sunday');
 
-    window.onscroll = function() {
-      if (window.pageYOffset >= stickyThreshold) {
-        navbar.classList.add('fixed_top');
-        navbar.style.bottom = 'auto'; // Remove bottom positioning when fixed at top
+let isTextAdded = false;
 
-         } else {
-        navbar.classList.remove('fixed_top');
-        navbar.style.bottom = '0'; // Ensure it stays at the bottom when not fixed at top
-      }
-    };
-  });
-
-  window.onscroll = function() {
-    
+trainings.addEventListener('click', function() {
+  if (isTextAdded) {
+    trainings.style.width = "";
+    monday.innerHTML = monday.innerHTML.replace(' Stadtpark', '');
+    friday.innerHTML = friday.innerHTML.replace(' Meerweinstraße 26', '');
+    sunday.innerHTML = sunday.innerHTML.replace(' Stadtpark', '');
+  } else {
+    trainings.style.width = "85%";
+    monday.innerHTML += ' Stadtpark';
+    friday.innerHTML += ' Meerweinstraße 26';
+    sunday.innerHTML += ' Stadtpark';
   }
+  // Toggle the flag
+  isTextAdded = !isTextAdded;
+});
